8c28ef7c0b00814f34076a9c3c61ad06bcd79f35
LilScottyPippen <lilscottypippen33@gmail.com>
2023-04-09 10:18:42 +0300

    Добавьте файлы проекта.

---
+++ HTML5/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace HTML5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelHTML5 = new System.Windows.Forms.Label();
+            this.tableMainPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxDesktop1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDesktop = new System.Windows.Forms.PictureBox();
+            this.artanPanel6 = new HTML5.ArtanPanel();
+            this.artanPanel4 = new HTML5.ArtanPanel();
+            this.artanPanel5 = new HTML5.ArtanPanel();
+            this.artanPanel3 = new HTML5.ArtanPanel();
+            this.artanPanel2 = new HTML5.ArtanPanel();
+            this.buttonArtan2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.artanPanel1 = new HTML5.ArtanPanel();
+            this.buttonArtan1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableMainPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDesktop1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDesktop)).BeginInit();
+            this.artanPanel2.SuspendLayout();
+            this.artanPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelHTML5
+            // 
+            this.labelHTML5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(243)))), ((int)(((byte)(249)))));
+            this.labelHTML5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelHTML5.Font = new System.Drawing.Font("Open Sans", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHTML5.Location = new System.Drawing.Point(0, 0);
+            this.labelHTML5.Name = "labelHTML5";
+            this.labelHTML5.Size = new System.Drawing.Size(1050, 65);
+            this.labelHTML5.TabIndex = 0;
+            this.labelHTML5.Text = "HTML5";
+            this.labelHTML5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tableMainPanel
+            // 
+            this.tableMainPanel.ColumnCount = 2;
+            this.tableMainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableMainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableMainPanel.Controls.Add(this.artanPanel6, 1, 2);
+            this.tableMainPanel.Controls.Add(this.artanPanel4, 1, 1);
+            this.tableMainPanel.Controls.Add(this.artanPanel5, 0, 2);
+            this.tableMainPanel.Controls.Add(this.artanPanel3, 0, 1);
+            this.tableMainPanel.Controls.Add(this.artanPanel2, 1, 0);
+            this.tableMainPanel.Controls.Add(this.artanPanel1, 0, 0);
+            this.tableMainPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableMainPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableMainPanel.Name = "tableMainPanel";
+            this.tableMainPanel.RowCount = 4;
+            this.tableMainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.11655F));
+            this.tableMainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.88345F));
+            this.tableMainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableMainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableMainPanel.Size = new System.Drawing.Size(1050, 635);
+            this.tableMainPanel.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableMainPanel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1050, 638);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBoxDesktop1
+            // 
+            this.pictureBoxDesktop1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxDesktop1.Image")));
+            this.pictureBoxDesktop1.Location = new System.Drawing.Point(25, 217);
+            this.pictureBoxDesktop1.Name = "pictureBoxDesktop1";
+            this.pictureBoxDesktop1.Size = new System.Drawing.Size(254, 257);
+            this.pictureBoxDesktop1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxDesktop1.TabIndex = 3;
+            this.pictureBoxDesktop1.TabStop = false;
+            // 
+            // pictureBoxDesktop
+            // 
+            this.pictureBoxDesktop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxDesktop.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxDesktop.Image")));
+            this.pictureBoxDesktop.Location = new System.Drawing.Point(784, 58);
+            this.pictureBoxDesktop.Name = "pictureBoxDesktop";
+            this.pictureBoxDesktop.Size = new System.Drawing.Size(217, 214);
+            this.pictureBoxDesktop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxDesktop.TabIndex = 4;
+            this.pictureBoxDesktop.TabStop = false;
+            // 
+            // artanPanel6
+            // 
+            this.artanPanel6.BackColor = System.Drawing.Color.White;
+            this.artanPanel6.BorderRadius = 30;
+            this.artanPanel6.ForeColor = System.Drawing.Color.Black;
+            this.artanPanel6.GradientAngle = 40F;
+            this.artanPanel6.GradientBottomColor = System.Drawing.Color.Lime;
+            this.artanPanel6.GradientTopColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.artanPanel6.Location = new System.Drawing.Point(528, 417);
+            this.artanPanel6.MaximumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel6.MinimumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel6.Name = "artanPanel6";
+            this.artanPanel6.Size = new System.Drawing.Size(200, 200);
+            this.artanPanel6.TabIndex = 13;
+            // 
+            // artanPanel4
+            // 
+            this.artanPanel4.BackColor = System.Drawing.Color.White;
+            this.artanPanel4.BorderRadius = 30;
+            this.artanPanel4.ForeColor = System.Drawing.Color.Black;
+            this.artanPanel4.GradientAngle = 150F;
+            this.artanPanel4.GradientBottomColor = System.Drawing.Color.Red;
+            this.artanPanel4.GradientTopColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.artanPanel4.Location = new System.Drawing.Point(528, 210);
+            this.artanPanel4.MaximumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel4.MinimumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel4.Name = "artanPanel4";
+            this.artanPanel4.Size = new System.Drawing.Size(200, 200);
+            this.artanPanel4.TabIndex = 12;
+            // 
+            // artanPanel5
+            // 
+            this.artanPanel5.BackColor = System.Drawing.Color.White;
+            this.artanPanel5.BorderRadius = 30;
+            this.artanPanel5.Dock = System.Windows.Forms.DockStyle.Right;
+            this.artanPanel5.ForeColor = System.Drawing.Color.Black;
+            this.artanPanel5.GradientAngle = 80F;
+            this.artanPanel5.GradientBottomColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.artanPanel5.GradientTopColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.artanPanel5.Location = new System.Drawing.Point(322, 417);
+            this.artanPanel5.MaximumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel5.MinimumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel5.Name = "artanPanel5";
+            this.artanPanel5.Size = new System.Drawing.Size(200, 200);
+            this.artanPanel5.TabIndex = 11;
+            // 
+            // artanPanel3
+            // 
+            this.artanPanel3.BackColor = System.Drawing.Color.White;
+            this.artanPanel3.BorderRadius = 30;
+            this.artanPanel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.artanPanel3.ForeColor = System.Drawing.Color.Black;
+            this.artanPanel3.GradientAngle = 90F;
+            this.artanPanel3.GradientBottomColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.artanPanel3.GradientTopColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.artanPanel3.Location = new System.Drawing.Point(322, 210);
+            this.artanPanel3.MaximumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel3.MinimumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel3.Name = "artanPanel3";
+            this.artanPanel3.Size = new System.Drawing.Size(200, 200);
+            this.artanPanel3.TabIndex = 10;
+            // 
+            // artanPanel2
+            // 
+            this.artanPanel2.BackColor = System.Drawing.Color.White;
+            this.artanPanel2.BorderRadius = 30;
+            this.artanPanel2.Controls.Add(this.buttonArtan2);
+            this.artanPanel2.Controls.Add(this.label4);
+            this.artanPanel2.ForeColor = System.Drawing.Color.Black;
+            this.artanPanel2.GradientAngle = 60F;
+            this.artanPanel2.GradientBottomColor = System.Drawing.Color.Lime;
+            this.artanPanel2.GradientTopColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.artanPanel2.Location = new System.Drawing.Point(528, 3);
+            this.artanPanel2.MaximumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel2.MinimumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel2.Name = "artanPanel2";
+            this.artanPanel2.Size = new System.Drawing.Size(200, 200);
+            this.artanPanel2.TabIndex = 8;
+            // 
+            // buttonArtan2
+            // 
+            this.buttonArtan2.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonArtan2.Location = new System.Drawing.Point(64, 158);
+            this.buttonArtan2.Name = "buttonArtan2";
+            this.buttonArtan2.Size = new System.Drawing.Size(75, 23);
+            this.buttonArtan2.TabIndex = 2;
+            this.buttonArtan2.Text = "Click";
+            this.buttonArtan2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Open Sans", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(43, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 31);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "LESSON 2";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // artanPanel1
+            // 
+            this.artanPanel1.BackColor = System.Drawing.Color.White;
+            this.artanPanel1.BorderRadius = 30;
+            this.artanPanel1.Controls.Add(this.buttonArtan1);
+            this.artanPanel1.Controls.Add(this.label3);
+            this.artanPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.artanPanel1.ForeColor = System.Drawing.Color.Black;
+            this.artanPanel1.GradientAngle = 190F;
+            this.artanPanel1.GradientBottomColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.artanPanel1.GradientTopColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.artanPanel1.Location = new System.Drawing.Point(322, 3);
+            this.artanPanel1.MaximumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel1.MinimumSize = new System.Drawing.Size(200, 200);
+            this.artanPanel1.Name = "artanPanel1";
+            this.artanPanel1.Size = new System.Drawing.Size(200, 200);
+            this.artanPanel1.TabIndex = 9;
+            // 
+            // buttonArtan1
+            // 
+            this.buttonArtan1.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonArtan1.Location = new System.Drawing.Point(62, 158);
+            this.buttonArtan1.Name = "buttonArtan1";
+            this.buttonArtan1.Size = new System.Drawing.Size(75, 23);
+            this.buttonArtan1.TabIndex = 1;
+            this.buttonArtan1.Text = "Click";
+            this.buttonArtan1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Open Sans", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(47, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "LESSON 1";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(243)))), ((int)(((byte)(249)))));
+            this.ClientSize = new System.Drawing.Size(1067, 561);
+            this.Controls.Add(this.pictureBoxDesktop);
+            this.Controls.Add(this.pictureBoxDesktop1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelHTML5);
+            this.MinimumSize = new System.Drawing.Size(1049, 600);
+            this.Name = "Form1";
+            this.tableMainPanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDesktop1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDesktop)).EndInit();
+            this.artanPanel2.ResumeLayout(false);
+            this.artanPanel2.PerformLayout();
+            this.artanPanel1.ResumeLayout(false);
+            this.artanPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHTML5;
+        private System.Windows.Forms.TableLayoutPanel tableMainPanel;
+        private System.Windows.Forms.Panel panel1;
+        private ArtanPanel artanPanel2;
+        private ArtanPanel artanPanel1;
+        private ArtanPanel artanPanel6;
+        private ArtanPanel artanPanel4;
+        private ArtanPanel artanPanel5;
+        private ArtanPanel artanPanel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonArtan1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonArtan2;
+        private System.Windows.Forms.PictureBox pictureBoxDesktop1;
+        private System.Windows.Forms.PictureBox pictureBoxDesktop;
+    }
+}
+
